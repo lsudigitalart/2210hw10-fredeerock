64a72032818bc1fb97a49f744cffaeae7221c4fa
Derick Ostrenko <dostrenko@gmail.com>
2016-10-25 10:26:21 -0500

    baby steps

--- sketch.js
+++ sketch.js
@@ -1,33 +1,16 @@
-// var x1 = 10;
-// var x2 = 20;
-// var x3 = 30;
-
-var x = [];
+var x = [-20, 20];
 
 function setup() {
   createCanvas(320, 240);
-  background(0);
-
-  for (var i = 0; i < 3000; i++) {
-    x[i] = round(random(-1000, 1000));
-  }
   println(x);
-
 }
 
 function draw() {
-  // x1 += 0.5;
-  // x2 += 0.5;
-  // x3 += 0.5;
+  background(0);
+  
+  x[0]++;
+  x[1]++;
 
-  // ellipse(x1, 20, 20);
-  // ellipse(x2, 50, 20);
-  // ellipse(x3, 100, 20);
-
-  for (var i = 0; i < x.length; i++) {
-    x[i] += 0.5;
-    var y = i * 0.4;
-    ellipse(x[i], y, 20);
-  }
-
+  ellipse(x[0], 20, 20);
+  ellipse(x[1], 50, 20);
 }
